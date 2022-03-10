64af0a52485ed050853d5774d4d9b25e24028641
Ayush <ayushthth@gmail.com>
2022-03-10 09:08:38 +0530

    FinalCommit

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 
 // Instead of dummy password use ur MongoDB Atlas credentials 
+// PASSWORD value can be changed in .env
+// .env file is ignored by git thus deliberately renamed it as .env.sample
 
 const DB='mongodb+srv://dbUser:'+process.env.PASSWORD+'@cluster0.45ipq.mongodb.net/hashIDE?retryWrites=true&w=majority&ssl=true'
 mongoose.connect(DB, {
