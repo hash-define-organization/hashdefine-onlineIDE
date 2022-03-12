ea0667787f4b82a086beaf7fc54ee9894355e1a5
Raghvendra Singh <raghvendrrsingh@gmail.com>
2022-03-12 21:06:05 +0530

    Added 404 page with well designed UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.scss';
 import React from 'react';
-import Header from './Components/Header/Header';
-import Main from './Components/Main/Main';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
-	render() {
-		return (
-			<div className="app">
-				<Header/>
-				<Main/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Components/Header/Header';
+import Main from '../Components/Main/Main';
+
+function Home() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Home;
