397d7c2d790628d6ec0842e8efff8837143c6f8c
Harshit <singhharshit552@gmail.com>
2022-03-14 17:38:00 +0530

    Variable name change,Added comments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 
+
+/* This is App class that calls Header and Main files, whereas CodeEditor and
+   Terminal files are called in Main folder, Main folder is present in Components 
+   folder */
 class App extends React.Component {
 	render() {
 		return (
--- src/Components/Terminal/Terminal.js
+++ src/Components/Terminal/Terminal.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './Terminal.scss';
 
+
+ /* Terminal class has two inner subTerminals one is for Input terminal where
+    user will enter the input, another one is for output, where they can view the result
+    of their desired input.*/
 class Terminal extends React.Component {
     
     render() {
 
         return (
             <div className={`terminal ${this.props.isCollapsed && 'collapsed'}`} ref = {this.terminal}>
-                <div className='subTerminal' style = {{ borderBottom: "1px solid white"}}>
-                    <div className='fieldName'>
+                <div className='subTerminalInput' style = {{ borderBottom: "1px solid white"}}>
+                    <div className='fieldNameInput'>
                         Enter Input 
                     </div>
-                    <textarea value={this.props.input} onChange={this.props.changeInput} className = "textField" placeholder='Input Goes Here ...' />
+                    <textarea value={this.props.input} onChange={this.props.changeInput} className = "textFieldInput" placeholder='Input Goes Here ...' />
                 </div>
-                <div className="subTerminal">
-                    <div className='fieldName'>Output</div>
-                    <textarea className = "textField" disabled value={this.props.output === "" ? this.props.error : this.props.output} />
+                <div className="subTerminalOutput">
+                    <div className='fieldNameOutput'>Output</div>
+                    <textarea className = "textFieldOutput" disabled value={this.props.output === "" ? this.props.error : this.props.output} />
                 </div>
             </div>
         )
