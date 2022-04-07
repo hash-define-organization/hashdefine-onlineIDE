de8cb520e70373b33974f6a0131c30beccaf5b9a
Ayush <ayushthth@gmail.com>
2022-04-07 19:02:25 +0530

    CRUD

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,22 +1,16 @@
-// const mongoose=require('mongoose')
-// require('dotenv').config()
-
-
-// // Instead of dummy password use ur MongoDB Atlas credentials 
-// // PASSWORD value can be changed in .env
-// // .env file is ignored by git thus deliberately renamed it as .env.sample
-
-// const DB='mongodb+srv://dbUser:'+process.env.PASSWORD+'@cluster0.45ipq.mongodb.net/hashIDE?retryWrites=true&w=majority&ssl=true'
-// mongoose.connect(DB, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })   
-//  .then(() => 
-//      console.log("DATABASE CONNECTED"))
-//    .catch(err => console.log(err));
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/hash-ide',{})
+require('dotenv').config()
 
-const connectToDatabase=()=>{console.log("Connected")}
 
-module.exports=connectToDatabase
+// Instead of dummy password use ur MongoDB Atlas credentials 
+// PASSWORD value can be changed in .env
+// .env file is ignored by git thus deliberately renamed it as .env.sample
+
+const DB='mongodb+srv://dbUser:'+process.env.PASSWORD+'@cluster0.45ipq.mongodb.net/hashIDE?retryWrites=true&w=majority&ssl=true'
+mongoose.connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })   
+ .then(() => 
+     console.log("DATABASE CONNECTED"))
+   .catch(err => console.log(err));
