dc20c1cbfa3aa9a5f96b16a4eb5d2e1ec23ef421
Ayush <ayushthth@gmail.com>
2022-04-07 19:02:02 +0530

    CRUD

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,16 +1,22 @@
+// const mongoose=require('mongoose')
+// require('dotenv').config()
+
+
+// // Instead of dummy password use ur MongoDB Atlas credentials 
+// // PASSWORD value can be changed in .env
+// // .env file is ignored by git thus deliberately renamed it as .env.sample
+
+// const DB='mongodb+srv://dbUser:'+process.env.PASSWORD+'@cluster0.45ipq.mongodb.net/hashIDE?retryWrites=true&w=majority&ssl=true'
+// mongoose.connect(DB, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })   
+//  .then(() => 
+//      console.log("DATABASE CONNECTED"))
+//    .catch(err => console.log(err));
 const mongoose=require('mongoose')
-require('dotenv').config()
+mongoose.connect('mongodb://127.0.0.1:27017/hash-ide',{})
 
+const connectToDatabase=()=>{console.log("Connected")}
 
-// Instead of dummy password use ur MongoDB Atlas credentials 
-// PASSWORD value can be changed in .env
-// .env file is ignored by git thus deliberately renamed it as .env.sample
-
-const DB='mongodb+srv://dbUser:'+process.env.PASSWORD+'@cluster0.45ipq.mongodb.net/hashIDE?retryWrites=true&w=majority&ssl=true'
-mongoose.connect(DB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })   
- .then(() => 
-     console.log("DATABASE CONNECTED"))
-   .catch(err => console.log(err));
+module.exports=connectToDatabase
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,24 +2,25 @@ const mongoose = require('mongoose')
 const express = require('express')
 const User = require('../models/User')
 const router = new express.Router()
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const auth = require('../middleware/auth')
+const { findByIdAndDelete } = require('../models/User')
 require('dotenv').config()
 
+
+// C: CREATE
 //create user(register)
 router.post('/registerUser', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-       const token = await user.generateAuthToken()
-       res.status(201).send({user})
-       res.redirect("/loginUser");
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user })
+        res.redirect("/loginUser");
     } catch (e) {
         res.status(400).send('UserName Already Taken!!')
     }
 })
-
-
 //user login
 router.post('/loginUser', async (req, res) => {
     try {
@@ -36,12 +37,44 @@ router.post('/loginUser', async (req, res) => {
     }
 })
 
-//Dummy protected route 
-router.get('/user',auth, async (req, res) => {
-    if(!req.user) {
+// R: Read
+//View Profile (Route under Authentication)
+router.get('/user', auth, async (req, res) => {
+    if (!req.user) {
         res.send("Something went wrong")
     }
-    res.send("This is a protected route")
+    res.send(req.user)
 })
+
+
+// U: Update
+// Update Profile (Route under Authentication)
+router.patch('/userUpdate', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['username', 'password', 'collegeName', 'LanguagesKnown']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// D: Delete
+//Delete User profile (Route under authentication)
+router.delete('/userDelete', auth, async (req, res) => {
+    if (!req.user) {
+        res.send("Something went wrong")
+    }
+    const user = await User.findByIdAndDelete(req.user._id);
+    res.send("Profile Deleted Successfully!!")
+})
+
+
 
 module.exports = router
